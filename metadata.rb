d92d4f8766bffb24d19f4f3f4a4a8d4f324dbd2c
David King <david@rexden.us>
2016-01-12 13:57:21 -0500

    Updating metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'david@rexden.us'
 license 'all_rights'
 description 'Installs/Configures rexbackup'
 long_description 'Installs/Configures rexbackup'
-version '0.1.0'
+version '0.1.1'
 
 depends 'sysctl'
 depends 'rexcore'
